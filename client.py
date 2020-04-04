ffcfbe1736226c4445d83a46537684df46ae6c19
8ctavio89 <60673282+8ctavio89@users.noreply.github.com>
2020-04-03 22:43:58 -0700

    Te devuelve un poderoso mensaje cuando pierdes.

--- client.py
+++ client.py
@@ -5,6 +5,7 @@ import server
 import copy
 import time
 
+
 def despliega_menu():
     try:
         print("--------------------------\n")
@@ -27,20 +28,29 @@ def despliega_menu():
 
 def faq():
     print("-¿Es esto realmente un FAQ?")
-    print("     No, son preguntas hechas y respondidas por Emilio. Por eso están mal respondidas. ")
+    print(
+        "     No, son preguntas hechas y respondidas por Emilio. Por eso están mal respondidas. "
+    )
     print("-¿De qué me sirve esto?")
     print("     No lo sé, sinceramente")
     print("-¿Cómo se juega?")
-    print("     Cuando entras al servidor por primera vez, se te asigna una mano automáticamente")
-    print("     si aún no tienes jugada tienes que presionar 1 para que se genere una mano")
+    print(
+        "     Cuando entras al servidor por primera vez, se te asigna una mano automáticamente"
+    )
+    print(
+        "     si aún no tienes jugada tienes que presionar 1 para que se genere una mano"
+    )
     print("-¿Cómo veo quién ganó?")
     print("     Selecciona la opción 3")
     print("-¿Por qué no puedo seleccionar mi mano?")
     print("     Es más divertido cuando se lo dejas al azar")
     print("¿Es este acaso un easter egg de Octavio?")
-    print("     La vida es dura, pero más dura es la verdura.")
+    print("     Profesor, ¿usted ha jugado DOOM?")
     print("-¿El Octavio roba FAQs?")
-    print("     Sí, este ni siquiera es tu Easter Egg")
+    print(
+        "     Sí, este ni siquiera es tu Easter Egg. Not really, pero mira, bueno, podría ser."
+        + " Estas son las mañanitas que cantaba el rey David..."
+    )
     print("\n")
 
 
@@ -48,22 +58,27 @@ def main(jugador):
     print("\n== JUEGO DE PIEDRA, PAPEL O TIJERA ==\n")
     print("Iniciando...\n")
     keyboardyes = KeyboardInterrupt
-    ip = 'localhost'
+    ip = "localhost"
     puerto = 9000
-    proxy = xmlrpc.client.ServerProxy('http://' + ip + ':' + str(puerto))
+    proxy = xmlrpc.client.ServerProxy("http://" + ip + ":" + str(puerto))
     try:
         opcion = 99
         cantidad_desconectados_pasada = 0
-        primera_vez = True  # para que no imprima quién se desconectó cuando recién entras
+        primera_vez = (
+            True  # para que no imprima quién se desconectó cuando recién entras
+        )
 
         if jugador in proxy.deck():  # si existe el jugador en el servidor
-            print("Ya existe el jugador:", jugador,
-                  "\nPor favor, reingresa con otro usuario")
+            print(
+                "Ya existe el jugador:",
+                jugador,
+                "\nPor favor, reingresa con otro usuario",
+            )
             opcion = 0
         else:
-            #print("Entraste como Observador")
+            # print("Entraste como Observador")
             j = proxy.agrega_jugador(jugador)
-            print("Tu mano es:", j[1])
+            print("Tu mano (" + (jugador) + ") es:", j[1])
 
         while opcion != 0:
 
@@ -75,8 +90,7 @@ def main(jugador):
             # esto es para que no imprima cada ciclo
             if cantidad_desconectados_pasada < cantidad_desconectados:
                 # esto no hace referencia a otro objeto, sino lo copia
-                cantidad_desconectados_pasada = copy.copy(
-                    cantidad_desconectados)
+                cantidad_desconectados_pasada = copy.copy(cantidad_desconectados)
                 if primera_vez == False:
                     print(mensaje)
 
@@ -93,31 +107,41 @@ def main(jugador):
                 print("Jugador(es):", n, "jugador(es).")
             elif opcion == 3:
                 d = proxy.deck()
-                #print(d)
-            
+                # print(d)
 
                 winner = ""
-                if (len(d) != 0 or len(d) != 1):
+                if len(d) != 0 or len(d) != 1:
                     # Aquí va el método/función que determina quien gana.
                     # Hay que meterlo en server.py para que el mensaje de ganador se comunique
                     # a todos los clientes.
 
-                    expected_value = next(iter(d.values())) #Toma un valor, ejemplo 'Piedra' en los valores del diccionario de jugadores.
-                    all_equal = all(value == expected_value for value in d.values()) #Verifica si es igual a todos los valores dentro del diccionario.
+                    expected_value = next(
+                        iter(d.values())
+                    )  # Toma un valor, ejemplo 'Piedra' en los valores del diccionario de jugadores.
+                    all_equal = all(
+                        value == expected_value for value in d.values()
+                    )  # Verifica si es igual a todos los valores dentro del diccionario.
 
-                    if (all_equal == True): #Si todos los valores son iguales, significa un empate entre todos.
+                    if (
+                        all_equal == True
+                    ):  # Si todos los valores son iguales, significa un empate entre todos.
                         print("Hubo un empate. Recomendamos reiniciar el juego.")
-                        
-                        for jugador, jugada in d.items(): #Este for imprime únicamente, con formato, la jugada de los jugadores.
-                            print(jugador, "con", jugada)                                
-                            '''
+
+                        for (
+                            jugador,
+                            jugada,
+                        ) in (
+                            d.items()
+                        ):  # Este for imprime únicamente, con formato, la jugada de los jugadores.
+                            print(jugador, "con", jugada)
+                            """
                             Ejemplo: 
                                 - Octavio con tijeras.
                                 - Emilio con tijeras.
                                 - JuanPi con piedra.
-                                                            '''
-                        time.sleep(3)    
-                        despliega_menu() #<-- Aquí podemos hacer que abra el menú después de un poco de tiempo.
+                                                            """
+                        time.sleep(3)
+                        despliega_menu()  # <-- Aquí podemos hacer que abra el menú después de un poco de tiempo.
                     else:
                         winner = proxy.definir_ganador()
                         if winner == "empate":
@@ -129,14 +153,25 @@ def main(jugador):
                                 for jugador in winner:
                                     print(jugador)
                             else:
+                                mano = proxy.deck()[jugador]
                                 print(f"Ganador: {winner[0]}")
+                                print("¡Has ganado!")
+                                if jugador not in winner:
+                                    proxy.desconectar_jugador(jugador)
+                                    print(
+                                        "Has sido enviado al lobby de los perdedores."
+                                        + " DOOM's music starts playing. Tarandarandararararan"
+                                    )
+                                    mensaje = proxy.checar_jugadores()
+                                    break
                 else:
                     print(
-                        "Sería recomendable hacer una jugada o agregar jugadores antes de jugar " +
-                          "o saber quién ganó.")
+                        "Sería recomendable hacer una jugada o agregar jugadores antes de jugar "
+                        + "o saber quién ganó."
+                    )
             elif opcion == 4:
                 mano = proxy.deck()[jugador]
-                print("Tu mano es:", mano)
+                print("Tu mano (" + (jugador) + ") es:", mano)
             elif opcion == 5:
                 faq()
             primera_vez = False
@@ -155,8 +190,9 @@ def main(jugador):
 
 if __name__ == "__main__":
     parse = argparse.ArgumentParser()
-    parse.add_argument("-j", "--jugador", dest="jugador",
-                       required=False, default="Fede")
+    parse.add_argument(
+        "-j", "--jugador", dest="jugador", required=False, default="Fede"
+    )
     args = parse.parse_args()
     jugador = args.jugador
     main(jugador)
